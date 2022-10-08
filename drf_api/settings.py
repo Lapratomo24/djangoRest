95d39ba7762082662fe6359d5ce0485ecd239ac7
Lapratomo24 <lapratomo24@yahoo.com>
2022-10-08 11:49:44 +0000

    last

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -56,10 +56,12 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOST = os.environ.get('ALLOWED_HOST')
-ALLOWED_HOSTS = ['localhost', ALLOWED_HOST]
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
+]
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
